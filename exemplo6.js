52480d52298071bb34fe473030d20d38aba045ae
Ludmila Maia <ludmilacmaia@gmail.com>
2022-04-14 20:45:13 -0300

    resolução do exemplo 6

--- exemplo6.js
+++ exemplo6.js
@@ -1,1 +1,8 @@
 //Crie uma função que recebe uma temperatura em graus celsius retorna o valor convertido em Fahrenheit.
+//F=C*9/5+32
+
+function conversorFarenheit (celsius){
+ const fahrenheit = celsius*9/5+32
+    return `${celsius}°C = ${fahrenheit}F`
+}
+console.log (conversorFarenheit(25))
