006e9aade0b297e129bcb9a9773ee4e6e0e8cbcd
Ludmila Maia <ludmilacmaia@gmail.com>
2022-04-14 21:27:25 -0300

    resolução do exemplo 9

--- exemplo9.js
+++ exemplo9.js
@@ -1,1 +1,9 @@
 //Crie uma função que irá converter uma quantia de real para dolar utilizando a cotação do dia.
+
+function conversorDolar (real){
+    const cotação = 4.70
+    const valorEmDolar = (real/cotação).toFixed(2)
+    return `R$ ${real} = ${valorEmDolar} dólares`
+
+}
+console.log(conversorDolar(100))
