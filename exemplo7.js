e0b594b672910486d1cc92c7ee55f2ef119a39ec
Ludmila Maia <ludmilacmaia@gmail.com>
2022-04-14 21:02:48 -0300

    resolução exemplo 7

--- exemplo7.js
+++ exemplo7.js
@@ -1,1 +1,8 @@
 //Crie uma função que recebe 2 parâmetros e retorna a multiplicação entre eles.
+
+function multiplica(num1,num2){
+    const resultadoMultiplicacao= num1 * num2
+    return `${num1} x ${num2} = ${resultadoMultiplicacao}`
+
+}
+console.log(multiplica(5,3))
