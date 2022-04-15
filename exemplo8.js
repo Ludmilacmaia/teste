7392ade07d064bf2c430d4792530122217a0d741
Ludmila Maia <ludmilacmaia@gmail.com>
2022-04-14 21:19:48 -0300

    resolução exemplo 8

--- exemplo8.js
+++ exemplo8.js
@@ -1,1 +1,7 @@
 //Crie uma função que calcula 5% de desconto retornando o valor do desconto.
+
+function valorDesconto (preço){
+    const desconto = preço*0.05
+    return `o valor do desconto é: R$${desconto}`
+}
+console.log(valorDesconto(100))
